311a66254821b1017ba53d8e2322c41757268aff
Otmane El bourki <oel-bour@e2r6p5.1337.ma>
2020-03-14 14:14:46 +0100

    bissmiallah

--- list_main.cpp
+++ list_main.cpp
@@ -14,6 +14,20 @@ int     main()
     ft::list<int>::iterator it = l.begin();
     // l.insert(it,10);
 
-    l.insert(it,5);
+    // l.insert(it,5);
+    for (size_t i = 0; i < 5; i++)
+    {
+        l.insert(it,9);
+        it++;
+    }
+    ft::list<int>::iterator it1 = l.begin();
+    ft::list<int>::iterator end = l.end();
+    for (; it1 != end; it1++)
+    {
+       cout << *it << endl;
+    }
+    // l.~list();
+    // list<int> t;
+    // t.insert()
     return 0;
 }
